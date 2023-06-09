129975d2cfd2aeada00ddaaf96332d914049d35d
lipem <lipem@MSI>
2023-06-09 15:55:57 +0100

    Instanciación de los menús que se irán creando.

--- src/ProyectoIng_Version8/Proyecto_Ing_V8_Main.java
+++ src/ProyectoIng_Version8/Proyecto_Ing_V8_Main.java
@@ -3,8 +3,19 @@ package ProyectoIng_Version8;
 public class Proyecto_Ing_V8_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		//Opciones de menú
+		System.out.println ("Seleccione un opción: ");
+		System.out.println (" [1] Recolectar parámetros de la red.");
+		System.out.println (" [2] Guardar parámetros en la base de datos.");
+		System.out.println (" [3] Visualizar base de datos.");
+		System.out.println (" [4] Buscar errores en la base de datos de la red.");
+		System.out.println (" [5] Hacer filtrado de la base de datos.");
+		
+		
+		
+		
+	}//Llave class
 
-	}
-
-}
+}//Llave main
